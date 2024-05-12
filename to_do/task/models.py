f79b18afc44b0a565cc4683fc88f8aea0c093400
Jerson Daza <Jersondaza04@gmail.com>
2024-05-11 21:34:38 -0500

    feat: first commit

---
+++ to_do/task/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Tarea(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    completada = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
